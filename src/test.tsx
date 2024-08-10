ba9a93f89d7fcfb7596c31339ce534f82a657592
emb3e <matthis.bauer@web.de>
2024-08-10 18:03:18 +0200

    working login with lambda counter

---
+++ src/test.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { get } from 'aws-amplify/api';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+
+Amplify.configure(config);
+
+function App({ signOut, user }) {
+    const [counter, setCounter] = useState(0); // Initialize counter state
+
+    async function incrementCounter() {
+        try {
+            const restOperation = get({
+                apiName: 'apitestgateway',
+                path: '/counter',
+                options: {
+                    queryParams: {
+                        counter: `${counter}`
+                    }
+                }
+            });
+            const response = await restOperation.response;
+            const {counter: fetchedCount} = await response.body.json();
+
+            setCounter(fetchedCount)
+            console.log('GET call succeeded: ', response);
+        } catch (e) {
+            console.log('GET call failed: ', JSON.parse(e.response.body.json()));
+        }
+    }
+
+    return (
+        <>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+
+            <div>
+                <h2>Counter: {counter}</h2> {/* Display the current counter value */}
+                <button onClick={incrementCounter}>Increment Counter</button> {/* Increment counter button */}
+            </div>
+        </>
+    );
+}
+
+const AppWithAuthenticator = withAuthenticator(App);
+
+export default AppWithAuthenticator;
